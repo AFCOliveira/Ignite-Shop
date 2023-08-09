44b6a8c7cc7071763a84ff6e6b736199f498d1f3
André Oliveira <132096135+AFCOliveira@users.noreply.github.com>
2023-08-08 21:42:07 -0300

    styling header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
 import { globalStyles } from "../styles/global"
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app"
+import { AppProps } from "next/app"
 
-globalStyles()
+export default function App({ Component, pageProps }: AppProps) {
+  globalStyles()
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
