1a6fdda11e81869010fc5a8b0bd9647ae9e64ed5
André Oliveira <132096135+AFCOliveira@users.noreply.github.com>
2023-08-08 22:12:35 -0300

    component img -> Image

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable @next/next/no-img-element */
 import { globalStyles } from "../styles/global"
 import logoImg from '../assets/logo.svg'
+import Image from 'next/image'
 import { Container, Header } from "../styles/pages/app"
 import { AppProps } from "next/app"
 
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
