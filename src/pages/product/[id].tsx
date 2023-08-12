27b56f0f973bc0ac043cda03809639c3c08c67fa
André Oliveira <132096135+AFCOliveira@users.noreply.github.com>
2023-08-11 22:39:26 -0300

    use callback

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,19 +1,33 @@
 import { useRouter } from 'next/router';
 import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { stripe } from '../../lib/stripe';
+import Stripe from 'stripe';
+import Image from 'next/image';
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageURL: string;
+    description: string;
+    price: string;
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+
   return (
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={product.imageURL} width={520} height={480} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus tempore inventore assumenda corrupti molestias pariatur labore asperiores nobis optio, dolorem nostrum dolor distinctio tenetur dolorum deserunt neque reiciendis aspernatur veniam.</p>
+        <p>{product.description}</p>
 
         <button>
           Comprar Agora
@@ -21,4 +35,37 @@ export default function Product() {
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageURL: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      }
+    },
+    revalidate: 60 * 60 * 1, // 1 hour  
+  }
 }
